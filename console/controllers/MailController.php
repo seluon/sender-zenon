00bc6c8011e8e1ecc8ac0374a1bbbb2b0bb74dbe
pugab <pugab@zenon.net>
2018-11-01 16:09:39 +0300

    Тестовый send перенесен в отдельный контроллер и добавлен в gitignore

--- console/controllers/MailController.php
+++ console/controllers/MailController.php
@@ -29,27 +29,4 @@ class MailController extends Controller
         $mail->makeQueueEmail($model->getArrayRecipient());
     }
 
-
-    public function actionSendTest()
-    {
-        $mail_array = [
-            'pugab@zenon.net'
-        ];
-        foreach ($mail_array as $recipient)
-        {
-            $result = Yii::$app->mailer->compose('mail_tpl2', [
-                'pin' => '745001',
-                'sale_pic' => '/app/sender-zen/common/mail/layouts/image/3tp_act.png',
-                'logo' => '/app/sender-zen/common/mail/layouts/image/logo.png',
-                'ico_hend' => '/app/sender-zen/common/mail/layouts/image/ico_hend.png',
-                'ico_shield' => '/app/sender-zen/common/mail/layouts/image/ico_shield.png',
-                'ico_symb' => '/app/sender-zen/common/mail/layouts/image/ico_symb.png'
-            ])
-                ->setFrom(['lesom@zenon.net' => 'Хостинг-провайдер Зенон Н.С.П'])
-                ->setTo($recipient)
-                ->setSubject("Специальное осеннее предложение от хостинг-провайдер \"Зенон Н.С.П.\"")
-                ->send();
-            var_dump($result);
-        }
-    }
 }
