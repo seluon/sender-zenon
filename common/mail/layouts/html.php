aa5200df667dd0e175e1cf2b1bf7a9b7b82aab09
pugab <pugab@zenon.net>
2018-11-01 13:52:01 +0300

    Изменен шаблон письма

--- common/mail/layouts/html.php
+++ common/mail/layouts/html.php
@@ -5,10 +5,11 @@ use yii\helpers\Html;
 /* @var $content string main view render result */
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ru">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
+    <meta name="viewport" content="initial-scale=1.0">
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
