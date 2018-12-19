9b6359b7275e2fd5cbb87f71c9b749a09c2ea2fe
pugab <pugab@zenon.net>
2018-12-19 10:46:03 +0300

    Добавляет экшн для загрузки емейлов из файла в базу

---
+++ console/controllers/AddbaseController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pugab
+ * Date: 19/12/2018
+ * Time: 10:38
+ */
+
+namespace console\controllers;
+
+
+use yii\console\Controller;
+
+class AddbaseController extends Controller
+{
+    public function actionAddMailFromFile($file)
+    {
+        $path = '/app/sender-zen/console/runtime/' . $file;
+        $arr = file($path);
+
+        foreach ($arr as $item) {
+            $item = strtolower($item);
+            $res[] = explode(" ", $item, 2);
+        }
+
+        foreach ($res as $key => $value)
+        {
+            $result[$value[0]] = explode(",", trim($value[1]));
+
+        }
+
+        foreach ($result as $pin => $email)
+        {
+
+            foreach ($email as $mail)
+            {
+                $search = Recipient::searchEmail($mail);
+                if(!$search) {
+                    $this->actionAddMailBase($pin, $mail);
+                }
+                continue;
+            }
+        }
+
+
+    }
+}
